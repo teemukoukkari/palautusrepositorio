1e1923d63f1e631f67be0dfc558bbc677fdbedb4
teemukoukkari <teemu.koukkari@gmail.com>
2024-11-10 17:29:05 +0200

    Add osamaara.py and edit index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -2,6 +2,7 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 from tulo import tulo
+from osamaara import osamaara
 
 logger("aloitetaan ohjelma")
 
@@ -10,6 +11,7 @@ y = int(input("luku 2: "))
 print(f"{x} + {y} = {summa(x, y)}") 
 print(f"{x} - {y} = {erotus(x, y)}") 
 print(f"{x} * {y} = {tulo(x, y)}") 
+print(f"{x} / {y} = {osamaara(x, y)}") 
 
 logger("lopetetaan")
 print("goodbye!")
