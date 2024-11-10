42187ab53aa32ecb3b794c16383cfeaf959f2c11
teemukoukkari <teemu.koukkari@gmail.com>
2024-11-10 16:15:26 +0200

    Week 2 ex 1-2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,21 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify_list(self, the_list):
+        return "\n- " + "\n- ".join(the_list) if len(the_list) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n\nAuthors: {self._stringify_list(self.authors)}"
+            f"\n\nDependencies: {self._stringify_list(self.dependencies)}"
+            f"\n\nDevelopment dependencies: {self._stringify_list(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
+import toml
 from project import Project
-
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,14 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parsed = toml.loads(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(
+            parsed['tool']['poetry']['name'],
+            parsed['tool']['poetry']['description'],
+            parsed['tool']['poetry']['license'],
+            parsed['tool']['poetry']['authors'],
+            list(parsed['tool']['poetry']['dependencies']),
+            list(parsed['tool']['poetry']['group']['dev']['dependencies'])
+        )
